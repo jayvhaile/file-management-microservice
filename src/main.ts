b01ddc92c2fa0820fd79eaef9616b35d7200c580
jayvhaile <jvhaileapps@gmail.com>
2023-10-11 20:28:26 +0300

    config adjustments

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = Number.parseInt(process.env.PORT?.toString() ?? "");
   if (Number.isNaN(port) || port < 0 || port > 65535)
-    throw new Error("No valid port provided");
+    throw new Error("No valid port is provided");
   await app.listen(port);
 }
 
